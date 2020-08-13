cd0ad1ea32b5d9cd78daf983aabaf0c43bc7239c
Piyumi047 <piyumidulanjali1998@gmail.com>
2020-08-13 21:40:01 +0530

    Guide_&_package_search_pages

---
+++ package_search_page.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Search_TourPackages</title>
+	<link rel="stylesheet" type="text/css" href="css/package_search_page.css">
+   <meta "name=viewport" content="width=device-width, user-scalable=no,initial-scable=1.0, maximum-scle=1.0, minimum-scalable=1.0">
+    <meta charset="utf-8">
+</head>
+<body>
+   <div class="header" >
+      <form>
+      	
+      	<div class="form-box">
+            <h1>Search Tour Package</h1>
+      		
+   				<input class="search destinations" type="text" name="destinations" id="" placeholder="Destinations">
+   			
+
+   				<input class="search arrivaldate" type="text" name="arrivaldate" id="" placeholder="Arrival_Date(DD/MM/YY)">
+   			
+
+   				<input class="search departuredate" type="text" name="departuredate" id="" placeholder="Departure_Date(DD/MM/YY)">
+   			
+   			
+   				<input class="search numoftourist" type="text" name="numoftourist" id="" placeholder="Number_Of_Tourist">
+
+   			
+   				<button class="Search-btn" type="button">Search</button>
+
+               <br><br>
+
+               <div class="check-box">
+      
+                  <input type="checkbox" name="" id="Round Trip Package">
+                  <label for="Round Trip Guides">Round Trip Package</label>
+
+                  <input type="checkbox" name="" id="Per-Day Package">
+                  <label for="Per-Day Guides">Per-Day Package</label>
+
+               </div><!--check-box-->
+                 <br><br><br><br>
+
+               <div class="Seeall-btn">
+                  <button  type="button">See All Packages</button>
+               </div><!--seeall-button-->
+      	</div><!--form-box-->
+
+
+      </form>
+   </div><!--header-->
+</body>
+</html>
