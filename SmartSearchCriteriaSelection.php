d2482fdac68709792130d8620c8b4dfb0f6c1cc6
sulakshanee <sulakshanee111@gmail.com>
2020-08-13 22:00:54 +0530

    SmartSearchCriteriaSelection

---
+++ SmartSearchCriteriaSelection.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Smart Search Criteria Selection</title>
+	<link rel="stylesheet" type="text/css" href="css/smartSearchCriteriaSelection.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body style="background-image: url('img/tree.jpg'); background-size:cover;background-position: center center;background-attachment: fixed; background-repeat:no-repeat;">
+
+	<button class="searchButton">Search</button>
+			
+	<div class="boxOutdoor clearfix">
+
+		<div class="name1">
+		
+			<h2>Outdoor Activities</h2>
+		</div>
+
+		<div class="box">
+				
+			<div class="checkbox1">
+				
+				<label class="container">
+					<input type="checkbox" > Bicycle Tour
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Go-Cart racing
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Kite Surfing
+				</label>
+			</div>	
+
+			<div class="checkbox1">
+
+				<label class="container">
+					<input type="checkbox" checked="checked"> Hiking
+				</label>
+				<label class="container">
+					<input type="checkbox" > Deep Sea Fishing
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Surfing
+				</label>
+			</div>	
+
+			<div class="checkbox1">	
+
+				<label class="container">
+					<input type="checkbox" > Whale watching
+				</label>	
+				<label class="container">
+					<input type="checkbox" checked="checked"> Golf
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Rock Climbing
+				</label>
+			</div>
+		</div>
+	</div>
+
+	<div class="boxIndoor clearfix">
+
+		<div class="name1">
+		
+			<h2>Indoor Activities</h2>
+		</div>
+
+		<div class="box">
+
+			<div class="checkbox1">
+				
+				<label class="container">
+					<input type="checkbox" checked="checked"> Museums
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Billiards
+				</label>
+				<label class="container">
+					<input type="checkbox" > Mosques
+				</label>
+			</div>	
+
+			<div class="checkbox1">
+				<label class="container">
+					<input type="checkbox" > Spa
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Tea Tasting
+				</label>
+				<label class="container">
+					<input type="checkbox" checked="checked"> Churches
+				</label>
+			</div>	
+
+			<div class="checkbox1">	
+				<label class="container">
+					<input type="checkbox" checked="checked"> Casino
+				</label>	
+				<label class="container">
+					<input type="checkbox" checked="checked"> Temples
+				</label>
+				<label class="container">
+					<input type="checkbox" > Hindu Shrines
+				</label>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
